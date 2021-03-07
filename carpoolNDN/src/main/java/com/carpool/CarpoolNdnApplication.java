b794db7879909db3b9285e4f0f9567ef7f15080d
nag <nageshramesh1998@gmail.com>
2021-03-07 14:08:34 +0530

    first commit

---
+++ carpoolNDN/src/main/java/com/carpool/CarpoolNdnApplication.java
@@ -0,0 +1,13 @@
+package com.carpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolNdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolNdnApplication.class, args);
+	}
+
+}
